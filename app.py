c3434c2d227977eda47d20e17cb9ab0608177173
666WannaCry666 <usov3124@gmail.com>
2022-01-10 14:04:54 +0700

    Help and Values commands

--- app.py
+++ app.py
@@ -6,6 +6,35 @@ TOKEN = "5002515778:AAEKU5QaeLu5wpcrDdcg_ZxbI3YlKnxHRjc"
 bot = telebot.TeleBot(TOKEN)
 
 
+keys = {
+    'рубль': 'RUB',
+    'гривна': 'UAH',
+    'доллар США': 'USD',
+    'евро': 'EUR',
+    'фунт стерлингов': 'GBP',
+    'иена': 'JPY',
+    'юань': 'CNY',
+    'шекель': 'ILS'
+}
+
+
 @bot.message_handler(commands=['start', 'help'])
-def echo_test(message: telebot.types.Message):
-    bot.send_message(message.chat.id, 'hello')
+def help(message: telebot.types.Message):
+    text = "Чтобы начать работу, введите комманду боту в следующем формате:\n \
+<имя валюты> \
+<в какую валюту перевести> \
+<количество переводимой валюты>\n \
+Чтобы узнать поддерживаемые валюты, введите комманду /values"
+
+    bot.reply_to(message, text)
+
+
+@bot.message_handler(commands=['values'])
+def values(message: telebot.types.Message):
+    text = "Доступные валюты: "
+    for key in keys.keys():
+        text = '\n'.join((text, key, ))
+    bot.reply_to(message, text)
+
+
+bot.polling()
